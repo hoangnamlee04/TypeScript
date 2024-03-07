d5cabfa2ce8b7865a9d43b3101934870fa24d791
hoangnamlee04 <150770315+hoangnamlee04@users.noreply.github.com>
2024-03-07 10:09:56 +0700

    day 2

---
+++ JS/TS/lesson_2/index.js
@@ -0,0 +1,13 @@
+"use strict";
+// Sử dụng ":" 
+const myMessage = "Xin chao tat ca chung may";
+const myAge = 30;
+const myMarried = false;
+const mySkills = ["html", "css", "js", "react", "driver"];
+const myJobs = ["developer", "teacher", "grap"];
+const myEducation = {
+    school: "FPOLY",
+    major: "IT",
+    graduatedYear: 2024,
+    GPA: 3.5,
+};
---
+++ TS/lesson_2/index.ts
@@ -0,0 +1,23 @@
+// Sử dụng ":" 
+
+const myMessage: string = "Xin chao tat ca chung may";
+
+const myAge: number =30;
+
+const myMarried: boolean = false;
+
+const mySkills:string[] = ["html", "css", "js", "react", "driver"]
+
+const myJobs: Array<string> = ["developer", "teacher", "grap"]
+
+const myEducation:{
+    school:string;
+    major:string;
+    graduatedYear:number;
+    GPA: number;
+} = {
+    school: "FPOLY",
+    major: "IT",
+    graduatedYear: 2024,
+    GPA: 3.5,
+}
